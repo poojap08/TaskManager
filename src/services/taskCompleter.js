17f5682eb247dc6f6bac8cbcec6e24f7d030fc16
poojap08 <poojap02810@gmail.com>
2025-04-10 01:13:50 +0530

    Update taskCompleter.js

--- src/services/taskCompleter.js
+++ src/services/taskCompleter.js
@@ -1,12 +1,12 @@
 const TaskFinder = require("./taskFinder");
 
 const TaskCompleter = {
-    markTaskComplete(id) {
-        const task = TaskFinder.getTaskById(id);
-        if (!task) return null;
-        task.completed = true;
-        return task;
-    }
+  markTaskComplete(id, userId) {
+    const task = TaskFinder.getTaskById(id, userId);
+    if (!task) return null;
+    task.completed = true;
+    return task;
+  }
 };
 
 module.exports = TaskCompleter;

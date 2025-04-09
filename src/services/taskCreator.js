58e35fdf8f9258fa74ce431ca3a608db5914d976
poojap08 <poojap02810@gmail.com>
2025-04-10 01:14:22 +0530

    Update taskCreator.js

--- src/services/taskCreator.js
+++ src/services/taskCreator.js
@@ -4,11 +4,11 @@ const tasks = require("./taskStorage");
 let taskId = 1;
 
 const TaskCreator = {
-    createTask(title, description) {
-        const task = new Task(taskId++, title, description);
-        tasks.push(task);
-        return task;
-    }
+  createTask(title, description, userId) {
+    const task = new Task(taskId++, title, userId, description);
+    tasks.push(task);
+    return task;
+  }
 };
 
 module.exports = TaskCreator;
